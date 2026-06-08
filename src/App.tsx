2b7dad480448c4af988839cf8fc915ef22254bc4
Alif Hafiz Kandayas <hafizmails615@gmail.com>
2026-06-08 12:36:37 +0700

    Add project page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HalideLanding from './components/ui/demo';
+import ProjectsPage from './components/ui/projects-page';
 
 function App() {
-  return <HalideLanding />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HalideLanding />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
